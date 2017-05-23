23eb6eb98f16f945e60b4c7e0cac260c32648f25
CaioTsubake <caiotsubake@gmail.com>
2017-05-23 00:23:31 -0300

    First Set of Fibonacci tests

---
+++ TDDProjects/FibonacciProjectTests/FibonacciUnitTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using FibonacciProject;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FibonacciProjectTests
+{
+    [TestClass]
+    public class FibonacciUnitTests
+    {
+        [TestMethod]
+        public void Fib_Given0_Return0()
+        {
+            int n = 0;
+
+            int result = Fibonacci.Fib(n);
+
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Fib_Given1_Return1()
+        {
+            //Assert
+            int n = 1;
+
+            //Act
+            int result = Fibonacci.Fib(n);
+
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void Fib_Given2_Return1()
+        {
+            //Assert
+            int n = 2;
+
+            //Act
+            int result = Fibonacci.Fib(n);
+
+            //Assert
+            Assert.AreEqual(result, 1);
+        }
+
+        [TestMethod]
+        public void Fib_Given3_Return2()
+        {
+            //Assert
+            int n = 3;
+
+            //Act
+            int result = Fibonacci.Fib(n);
+
+            //Assert
+            Assert.AreEqual(result, 2);
+        }
+
+        [TestMethod]
+        public void Fib_Given4_Return3()
+        {
+            //Assert
+            int n = 4;
+
+            //Act
+            int result = Fibonacci.Fib(n);
+
+            //Assert
+            Assert.AreEqual(result, 3);
+        }
+    }
+}
---
+++ TDDProjects/FibonacciProjectTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FibonacciProjectTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FibonacciProjectTests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c6ad8912-370a-4864-9a61-21937edf0dc0")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
